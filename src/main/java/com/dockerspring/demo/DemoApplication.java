f79e09aedcaddf8f86cc08f7bd7c8e62ea25a9b0
Sathwik <sathwikchirivelli@gmail.com>
2023-10-04 12:10:32 +0530

    Initial commit

---
+++ src/main/java/com/dockerspring/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.dockerspring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("message")
+	public String message() {
+		return "Docker SpringBoot App";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
